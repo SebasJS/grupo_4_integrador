166fa852610de77052c61099abeac77624ede57a
juan salazar <juancamilosalazarrestrepo@gmail.com>
2021-12-20 23:20:37 -0500

    servidor nav y banner
    iniciamos el proyecto con nodejs instalamos express y creamos el nav y el banner  con html y css

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+const publicPath = path.resolve(__dirname,'./public');
+
+app.use(express.static(publicPath));
+
+app.get('/', (req,res) => res.sendFile(path.resolve(__dirname, './views/home.html'))  )
+
+app.listen(process.env.PORT || 3000, () =>console.log("Servidor Corriendo en Puerto 3000"));

19a453d9dca53b1369985efe29c2609aaedc20eb
SebasJS <gomezvargas97@gmail.com>
2021-12-23 15:52:28 -0500

    creacion-product-car
    Creacion de pagina de carrito de productos

--- app.js
+++ app.js
@@ -13,3 +13,4 @@ app.get('/', (req,res) => res.sendFile(path.resolve(__dirname, './views/home.htm
 app.listen(process.env.PORT || 3000, () =>console.log("Servidor Corriendo en Puerto 3000"));
 
 app.get('/product-info',(req,res)=> res.sendFile(path.resolve(__dirname,'./views/product-info.html')))
+app.get('/product-car',(req,res)=> res.sendFile(path.resolve(__dirname,'./views/product-car.html')))

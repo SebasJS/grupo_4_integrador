66b9312c529dcbc3d12b781314ab51942caa706c
Manuel Ropero <maroperom@correo.udistrital.edu.co>
2022-01-20 14:56:44 -0500

    Se implemento el Create para los productos

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -1,6 +1,12 @@
+const fs = require('fs');
+const path = require('path');
 
+const productsFilePath = path.join(__dirname, '../database/productos.JSON');
+const productos = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
 module.exports = {
     home: (req,res)=>{
-        res.render(("main/home"));
+        res.render(("main/home"),{
+            productos
+        });
     }
 }
--- src/routes/adminRouter.js
+++ src/routes/adminRouter.js
@@ -1,6 +1,16 @@
 const express = require("express");
 const router = express.Router();
-
+const multer = require('multer');
 const adminController = require("../controllers/adminController");
+let storage = multer.diskStorage({
+    destination: (req,file,cb) => cb(null, "public/img"),
+    filename : (req, file , cb) => cb(null,Date.now()+ "-" +file.originalname)
+});
+let upload = multer ({storage})
+
+
+
 router.get('/create',adminController.create);
+router.post('/', upload.single("image"),adminController.store);
+
 module.exports = router;

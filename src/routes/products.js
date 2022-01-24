497ee28fb1104a8fdcedb67bf68cbe8ac314cfdf
juan salazar <juancamilosalazarrestrepo@gmail.com>
2022-01-23 22:03:18 -0500

    proyecto ecommerce con CRUD

---
+++ src/routes/products.js
@@ -0,0 +1,10 @@
+// ************ Require's ************
+const express = require("express");
+const router = express.Router();
+
+// ************ Controller Require ************
+const productsController = require("../controllers/productsController");
+
+
+/*** GET ALL PRODUCTS ***/
+router.get("/", productsController.index);

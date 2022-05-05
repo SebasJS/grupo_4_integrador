b4b943518e9a8d8f0d4693ba08de95139d8440f7
juan salazar <juancamilosalazarrestrepo@gmail.com>
2022-05-04 23:34:00 -0500

    validator

--- src/routes/adminRouter.js
+++ src/routes/adminRouter.js
@@ -5,6 +5,7 @@ const adminController = require("../controllers/adminController");
 const adminUsersController = require("../controllers/adminUsersController");
 const guestMiddleware = require('../middlewares/guestMiddleware');
 const authMiddleware = require('../middlewares/authMiddleware');
+const { validateRegister } = require('../validators/register');
 let storage = multer.diskStorage({
     destination: (req,file,cb) => cb(null, "public/img"),
     filename : (req, file , cb) => cb(null,Date.now()+ "-" +file.originalname)
@@ -14,7 +15,7 @@ let upload = multer ({storage})
 
 
 router.get('/create',adminController.create);
-router.post('/', upload.single("image"),adminController.store);
+router.post('/', upload.single("image"), adminController.store);
 
 //router.get('/update/:id', adminController.update); YA ESTABA
 //router.get('/edit/:id', adminController.edit); 
@@ -28,10 +29,10 @@ router.delete('/delete/:id', adminController.delete);
 //Rutas para CRUD de Usuarios por parte del ADMIN
 
 router.get('/users', adminUsersController.index);
-/*router.get('/addUsers', adminUsersController.add);
-router.post('/users/create', adminUsersController.create);
+router.get('/addUsers', adminUsersController.add);
+router.post('/users/create', validateRegister, adminUsersController.create);
 router.get('/users/edit/:id', adminUsersController.edit);
-router.post('/users/update/:id',upload.single("image"), adminUsersController.update);*/
+router.post('/users/update/:id',upload.single("image"), adminUsersController.update);
 router.post('/users/delete/:id', adminUsersController.delete);
 
 //Rutas para el login de usuarios
---
+++ src/validators/register.js
@@ -0,0 +1,18 @@
+const { check } = require('express-validator');
+const { validateResult } = require('../helpers/validateHelper');
+
+const validateRegister = [
+    check('name').exists().notEmpty().withMessage('*Tienes que ingresar un nombre'),
+    check('email').exists().isEmail().withMessage('Debe ser un email valido'),
+    check('password').exists().isLength({min:8}).withMessage('la contraseña debe tener minimo 8 digitos'),
+    check('phone').isLength({min:11}).withMessage('El numero debe tener mas de 10 digitos'),
+    check('card').exists().isLength({min:10}).withMessage('la tarjeta debe tener minimo 10 digitos'),
+    check('imagen').exists().notEmpty().withMessage('debe ingresar una imagen'),
+    check('direccion').exists().notEmpty().withMessage('debe ingresar una direccion'),
+    check('departamentoId').exists().withMessage('debe elegir un departamento'),
+    (req, res, next) => {
+        validateResult(req, res, next);
+    }
+]
+
+module.exports = { validateRegister };
